7d0ffd279f0be6025ced7a4143334383aa928209
Nik Conwell <nikconwell@gmail.com>
2019-09-16 15:56:45 -0400

    Add in running state.  Also write guesses to file so can see how things progress (chart)

--- test.py
+++ test.py
@@ -14,15 +14,33 @@ def derivative(x):
 
 # Our inputs.  Each set of 3 inputs maps to a single output (see output_dataset)
 input_dataset = np.array([  [0,0,1],
+                            [0,1,0],
                             [0,1,1],
+                            [1,0,0],
                             [1,0,1],
+                            [1,1,0],
                             [1,1,1] ])
     
 # Outputs we expect for the 4 inputs.            
 output_dataset = np.array([ [0],
+                            [1],
+                            [1],
+                            [0],
                             [0],
                             [1],
                             [1]  ])
+
+#input_dataset = np.array([  [0,0],
+#                             [0,1],
+#                             [1,0],
+#                             [1,1] ])
+
+# output_dataset = np.array([ [0],
+#                             [1],
+#                             [0],
+#                             [1]   ])
+
+
 
 # seed random numbers to make calculations the same every run - makes comparing runs easier since always get same values
 np.random.seed(1)
@@ -31,35 +49,52 @@ np.random.seed(1)
 # 3 X 1 matrix, numbers ranging from -1 to 1.
 #synapse0 = 2*np.random.random((3,1)) - 1
 #synapse0 = np.array([[-0.16595599],[0.44064899],[-0.99977125]])
-s1 = np.array([[.9],[-.9],[.9]])
+w1 = np.array([[.9],[-.9],[.9]])
+#w1 = np.array([[.9],[-.9]])
 
 # So the idea of the work is that we take the input_dataset, adjust it by the synapse0 weights (multiply)
 # and see how close it is to the desired output.  How far off it is from the output is the error and we
 # adjust the weights by that derivative of that amount (so a small adjust).  And then repeat for 10,000 times.
 
-file_l1 = open("l1.csv","w+")
-#file_s1 = open("s1.csv","w+")
+#file_l1 = open("l1.csv","w+")
+#file_w1 = open("w1.csv","w+")
 
-for iter in range(10000):
+for iter in range(100):
 
     # forward propagation, multiply input matrix by weights matrix to figure out layer1 values.
-    l1 = sigmoid(np.dot(input_dataset,s1))
+    l1 = sigmoid(np.dot(input_dataset,w1))
 
-    # how much did we miss?
+    # Figure out how far off we were.
     l1_error = output_dataset - l1
 
-    # multiply how much we missed by the 
-    # slope of the sigmoid at the values in l1
+    # Figure out how much we will change our weights by.  Too little here and it will take a ton
+    # of iterations to converge.  Too much change and we lack resolution and may miss the target
+    # by too much and then will adjust back by that.
     l1_delta = l1_error * derivative(l1)
 
     # update weights
-    s1 += np.dot(input_dataset.T,l1_delta)
+    w1 += np.dot(input_dataset.T,l1_delta)
 
-    file_l1.write("{}\t{}\t{}\t{}\n".format(l1[0][0],l1[1][0],l1[2][0],l1[3][0]))
-#    file_s1.write("{}\t{}\t{}\t{}\n".format(s1[0][0],s1[1][0],s1[2][0],s1[3][0]))
+    #file_l1.write("{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\n".format(l1[0][0],l1[1][0],l1[2][0],l1[3][0]))
+    #file_w1.write("{:.5f}\t{:.5f}\t{:.5f}\n".format(w1[0][0],w1[1][0]))
+    
+    # Show current state of the learning along with the weights:
+    
+    print("\r",end='')
+    for item in l1:
+        print("{:05.5f}  ".format(item[0]),end='')
+    print("    Weights: ",end='')
+    for item in w1:
+        print("{:05.5f}  ".format(item[0]),end='')
 
-print("Output After Training:")
-print(l1)
+print()
+print()
 
-file_l1.close()
-#s1.close()
+print("Goal:")
+for item in output_dataset:
+    print("{:05.5f}  ".format(item[0]),end='')
+
+print()
+print()
+#file_l1.close()
+#file_w1.close()

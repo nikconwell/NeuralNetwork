db10bb6d7e5a26ce590ac3eaa732b025ecb24509
Nik Conwell <nikconwell@gmail.com>
2019-09-17 08:03:08 -0400

    Simplified test cases.  Added better csv log data

--- test.py
+++ test.py
@@ -12,54 +12,51 @@ def derivative(x):
     return x*(1-x)
 
 
+#
+# Stuff we are learning.  We need to convert the input_dataset into the
+# output dataset.  So we learn the weights or conversion factors that work to convert
+# each of the 3 test cases into the output case.
+#
 # Our inputs.  Each set of 3 inputs maps to a single output (see output_dataset)
 input_dataset = np.array([  [0,0,1],
                             [0,1,0],
-                            [0,1,1],
-                            [1,0,0],
-                            [1,0,1],
-                            [1,1,0],
-                            [1,1,1] ])
+                            [1,0,0]])
     
-# Outputs we expect for the 4 inputs.            
+# Outputs we expect for the inputs.            
 output_dataset = np.array([ [0],
                             [1],
-                            [1],
-                            [0],
-                            [0],
-                            [1],
-                            [1]  ])
+                            [0]  ])
 
-#input_dataset = np.array([  [0,0],
-#                             [0,1],
-#                             [1,0],
-#                             [1,1] ])
+print("Inputs:")
+print(input_dataset)
+print()
+print("Expected Output:")
+print(output_dataset)
+print()
+print()
 
-# output_dataset = np.array([ [0],
-#                             [1],
-#                             [0],
-#                             [1]   ])
+#
+# Our 3 neuron neural network.  Seed it with some values, doesn't matter what, could be random.  These are the weights
+# that we will adjust each iteration until we get closer and closer to the output_dataset values.
+#
+w1 = np.array([[-.9],[.0],[.9]])
 
-
-
-# seed random numbers to make calculations the same every run - makes comparing runs easier since always get same values
-np.random.seed(1)
-
-# Random weights of synapse connections between input and output layer.
+# Could also do random seed:
+# np.random.seed(1)
 # 3 X 1 matrix, numbers ranging from -1 to 1.
 #synapse0 = 2*np.random.random((3,1)) - 1
-#synapse0 = np.array([[-0.16595599],[0.44064899],[-0.99977125]])
-w1 = np.array([[.9],[-.9],[.9]])
-#w1 = np.array([[.9],[-.9]])
 
 # So the idea of the work is that we take the input_dataset, adjust it by the synapse0 weights (multiply)
 # and see how close it is to the desired output.  How far off it is from the output is the error and we
 # adjust the weights by that derivative of that amount (so a small adjust).  And then repeat for 10,000 times.
 
-#file_l1 = open("l1.csv","w+")
-#file_w1 = open("w1.csv","w+")
+#
+# We will log our learning findings at each step.
+#
+file_results = open("results.csv","w+")
+file_results.write("iter\tw1\tw2\tw3\tl1\tl2\tl3\n")
 
-for iter in range(100):
+for iter in range(1000):
 
     # forward propagation, multiply input matrix by weights matrix to figure out layer1 values.
     l1 = sigmoid(np.dot(input_dataset,w1))
@@ -78,23 +75,18 @@ for iter in range(100):
     #file_l1.write("{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\n".format(l1[0][0],l1[1][0],l1[2][0],l1[3][0]))
     #file_w1.write("{:.5f}\t{:.5f}\t{:.5f}\n".format(w1[0][0],w1[1][0]))
     
-    # Show current state of the learning along with the weights:
-    
+    # Show current state of the learning.
+    # Iteration:
     print("\r",end='')
-    for item in l1:
-        print("{:05.5f}  ".format(item[0]),end='')
-    print("    Weights: ",end='')
+    print("Iteration: {:7,}   ".format(iter),end='')
+    print("Weights: ",end='')
     for item in w1:
         print("{:05.5f}  ".format(item[0]),end='')
+    print("Learned: ",end='')
+    for item in l1:
+        print("{:05.5f}  ".format(item[0]),end='')
+
+    file_results.write("{}\t{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\n".format(iter,w1[0][0],w1[1][0],w1[2][0],l1[0][0],l1[1][0],l1[2][0]))
 
 print()
-print()
-
-print("Goal:")
-for item in output_dataset:
-    print("{:05.5f}  ".format(item[0]),end='')
-
-print()
-print()
-#file_l1.close()
-#file_w1.close()
+file_results.close()

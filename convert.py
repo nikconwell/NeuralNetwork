408d9ce3123fbca92b9e5a096cc756626dd27ade
Nik Conwell <nikconwell@gmail.com>
2019-09-19 09:12:21 -0400

    Changed matrix to be 32 bits long for the word since I think will be better for a 32 bit neural network

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ for character in word:
     print (">{}< ({} - {:08b})".format(character,ord(character),ord(character)))
 
 # Create a bit array based on length of word * 8 for 8 bits per character.
-bitarray = np.zeros((len(word),8))
+bitarray = np.zeros((len(word)*8,1))
 
 char_index=0
 bit_index=0
@@ -21,7 +21,7 @@ for character in word:
     # Look at each bit in the character
     bit_index = 0
     for bit in "{:08b}".format(ord(character)):
-        bitarray[char_index][bit_index] = bit
+        bitarray[(char_index*8)+bit_index][0] = bit
         bit_index += 1
     char_index += 1
 
@@ -30,16 +30,17 @@ print("bitarray:\n",bitarray)
 #
 # Convert back to characters.
 #
+index=0
 character=0
 string=""
-for byte in bitarray:
-    index = 0
-    for bit in byte:
-        if (bit == 1):
-            character |= (1 << (7 - index))
-        index += 1
-    print(">{}< ({:c})".format(character,character))
-    string += "{:c}".format(character)
-    character = 0
+for bit in bitarray:
+    if (bit[0] == 1):
+        character |= (1 << (7 - index))
+    index += 1
+    if (index == 8):
+        string += "{:c}".format(character)
+        print(">{}< ({:c})".format(character,character))
+        character = 0
+        index = 0
 
 print("output: ",string)

cbd4c865ae776cf7987ba6d8dcfb63fabbda8dca
Nik Conwell <nikconwell@gmail.com>
2019-09-19 08:17:53 -0400

    Converted to use a matrix

--- convert.py
+++ convert.py
@@ -12,32 +12,34 @@ for character in word:
     print (">{}< ({} - {:08b})".format(character,ord(character),ord(character)))
 
 # Create a bit array based on length of word * 8 for 8 bits per character.
-bitarray = np.zeros(len(word)*8)
+bitarray = np.zeros((len(word),8))
 
-index=0
+char_index=0
+bit_index=0
 # Look at each character in the string
 for character in word:
     # Look at each bit in the character
+    bit_index = 0
     for bit in "{:08b}".format(ord(character)):
-        bitarray[index] = bit
-        index += 1
+        bitarray[char_index][bit_index] = bit
+        bit_index += 1
+    char_index += 1
 
 print("bitarray:\n",bitarray)
 
 #
 # Convert back to characters.
 #
-index=0
 character=0
 string=""
-for bit in bitarray:
-    if (bit == 1):
-        character |= (1 << (7 - index))
-    index += 1
-    if (index == 8):
-        print(">{}< ({:c})".format(character,character))
-        string += "{:c}".format(character)
-        character = 0
-        index = 0
+for byte in bitarray:
+    index = 0
+    for bit in byte:
+        if (bit == 1):
+            character |= (1 << (7 - index))
+        index += 1
+    print(">{}< ({:c})".format(character,character))
+    string += "{:c}".format(character)
+    character = 0
 
 print("output: ",string)
